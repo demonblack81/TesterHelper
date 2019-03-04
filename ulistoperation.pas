e5b1d1aa39cb76b32fa3fd50dab01b15fccc5a6b
demonblack81 <demonback@tut.by>
2019-03-04 09:26:16 +0300

    Добавилновый мудуль для функций

---
+++ ulistoperation.pas
@@ -0,0 +1,23 @@
+unit uListOperation;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+function LoadNameListInString(Path:string): TStringList;
+
+implementation
+
+function LoadNameListInString(Path: string): TStringList;
+var FileSearch: TSearchRec;
+    Attr: Integer;
+    i: integer;
+begin
+
+end;
+
+end.
+
