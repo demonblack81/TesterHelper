fcdb10518dcf09750e6535256d2150b039c250e7
demonblack81 <demonback@tut.by>
2019-03-17 02:03:09 +0300

    Добавил несколько процедур и добавил иконку добавления слов в лист

--- ueditstringsparam.pas
+++ ueditstringsparam.pas
@@ -16,15 +16,21 @@ type
 
   TStringsParamForm = class(TForm)
     AddNameBitBtn: TBitBtn;
+    BtnClose: TButton;
+    BtnSave: TButton;
     DelNameBitBtn: TBitBtn;
     AddListBtn: TButton;
     DelListBtn: TButton;
+    Label2: TLabel;
     NameListComboBox: TComboBox;
     Label1: TLabel;
     WordListBox: TListBox;
     UpDown1: TUpDown;
+    procedure AddListBtnClick(Sender: TObject);
+    procedure AddNameBitBtnClick(Sender: TObject);
+    procedure BtnCloseClick(Sender: TObject);
+    procedure BtnSaveClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure NameListComboBoxChange(Sender: TObject);
     procedure NameListComboBoxCloseUp(Sender: TObject);
   private
     ListDir: string;
@@ -59,19 +65,49 @@ begin
   end else begin
     // Произошла ошибка нужна выводить мисагу что случилось
   end;
-
 end;
 
-procedure TStringsParamForm.NameListComboBoxChange(Sender: TObject);
+procedure TStringsParamForm.BtnCloseClick(Sender: TObject);
 begin
+  StringsParamForm.Close;
+end;
 
+procedure TStringsParamForm.BtnSaveClick(Sender: TObject);
+begin
+  WordListBox.Items.SaveToFile(ListDir+NameListComboBox.items[NameListComboBox.ItemIndex]);
+end;
+
+procedure TStringsParamForm.AddListBtnClick(Sender: TObject);
+var NewName: string;
+begin
+  NewName := '';
+  if not InputQuery('Название листа фраз:', 'Введите имя для листа фраз', NewName ) then exit;
+  if NewName <> '' then NameListComboBox.Items.Add(NewName);
+  FileCreate(ListDir+NewName);
+end;
+
+procedure TStringsParamForm.AddNameBitBtnClick(Sender: TObject);
+var NewWord: string;
+begin
+  NewWord := '';
+  if not InputQuery('Веведите слово:', 'Введите слова для листа фраз', NewWord) then exit;
+  if NewWord <> '' then WordListBox.Items.Add(NewWord);
 end;
 
 procedure TStringsParamForm.NameListComboBoxCloseUp(Sender: TObject);
+var FileSearch: TSearchRec;
+    Attr : integer;
 begin
   If NameListComboBox.ItemIndex <> -1 then begin
-    WorldlistBox.Items.Clear;
-    WordListBox.Items.LoadFromFile(ListDir + NameListComboBox.items[NameListComboBox.ItemIndex]);
+    WordListBox.Items.Clear;
+    if FindFirst(ListDir + NameListComboBox.items[NameListComboBox.ItemIndex], Attr, FileSearch) = 0 then begin
+      if FileSearch.Size > 0 then begin
+        WordListBox.Items.LoadFromFile(ListDir + NameListComboBox.items[NameListComboBox.ItemIndex]);
+      end else begin
+       ShowMessage('Выбранный файл пуст.');
+      end;
+    end;
+    FindClose(FileSearch);
   end;
 end;
 
