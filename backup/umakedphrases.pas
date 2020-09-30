1191c19f438c3cb08dca72a211d445f641837596
demonblack81 <demonblack81@tut.by>
2020-09-30 22:42:24 +0300

    Добавил форму для списка созданных фраз

---
+++ backup/umakedphrases.pas
@@ -0,0 +1,33 @@
+unit uMakedPhrases;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    AddPhraseFromClipboardBitBtn: TBitBtn;
+    CopySelectedPraseBitBtn: TBitBtn;
+    CloseFprmBitBtn: TBitBtn;
+    MakedPrasesListBox: TListBox;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
